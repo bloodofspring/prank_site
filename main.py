b5f70f7bff2c35c0fd6d77603204ff45a852e06c
bloodofspring <t.shagin@gmail.com>
2024-11-03 18:37:42 +0300

    feat: Исправлена ошибка с отсутствием имени пользователя бота

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ app = Flask(__name__)
 def home(u_id):
     try:
         load_dotenv()
-        telebot.TeleBot(environ["bot_token"]).send_message(int(u_id), "Кто-то перешел по пранк-ссылке!")
+        bot = (telebot.TeleBot(environ["bot_token"]))
+        bot.send_message(int(u_id), "Кто-то перешел по пранк-ссылке!")
+        bot_username = bot.get_me().username
     except:
-        pass
-    return render_template("main.html", u_id=u_id)
+        bot_username="<error>"
+
+    return render_template("main.html", u_id=u_id, bot_username=bot_username)
 
 
 if __name__ == "__main__":
