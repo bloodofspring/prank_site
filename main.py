bb81b49565ed82f2e7783a52c6cd549f60b34f67
bloodofspring <t.shagin@gmail.com>
2024-11-03 14:45:19 +0300

    feat: добавлена главная страница

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/promos/<user_id>")  # promos
-def home(user_id):
+@app.route("/")  # promos
+def home():
     return render_template("index.html")
 
 
