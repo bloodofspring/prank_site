f3efbedf7c4c4a20713c0f93b0c9fd4eb0500cda
bloodofspring <t.shagin@gmail.com>
2024-11-03 15:04:25 +0300

    feat: промежуточный коммит

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")  # promos
-def home():
-    return render_template("index.html")
+@app.route("/<u_id>")
+def home(u_id):
+    return render_template("main.html", u_id=u_id)
+
+
+@app.route("/popalsa/<u_id>")
+def prank(u_id):
+    return render_template("prank.html", bot_username="testing_158258358_bot")
 
 
 if __name__ == "__main__":
